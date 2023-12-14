db2b9a1d87ff66fecb5dc026ee0e5975f80776b9
Mark Turchik <mark.turchik@outlook.com>
2023-12-14 17:30:43 -0600

    Add CORS policy for local and GitHub pages

--- WarcraftGearPlanner.Server/Program.cs
+++ WarcraftGearPlanner.Server/Program.cs
@@ -12,6 +12,15 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 	options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 	options.JsonSerializerOptions.NumberHandling = JsonNumberHandling.AllowReadingFromString;
 });
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("CorsPolicy", policy =>
+	{
+		policy.WithOrigins("http://localhost:4200/", "https://mturchik.github.io/WarcraftGearPlanner/")
+			.AllowAnyMethod()
+			.AllowAnyHeader();
+	});
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
