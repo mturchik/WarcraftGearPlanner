79f9176ac2a5194a1bf2fa73f45106378c5f5263
mturchik <46897798+mturchik@users.noreply.github.com>
2023-12-24 17:50:59 -0600

    Update version number of Server and Functions (#15)
    Add Status endpoint to Functions

---
+++ WarcraftGearPlanner.Functions/Functions/Status.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace WarcraftGearPlanner.Functions.Functions;
+
+public static class Status
+{
+	[FunctionName("Status")]
+	public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req, ILogger log)
+	{
+		log.LogInformation("C# HTTP trigger function processed a request.");
+
+		var version = Assembly.GetExecutingAssembly().GetName().Version;
+		return new OkObjectResult($"APPLICATION RUNNING - v{version}");
+	}
+}
