d70bbdd2a1649a988b94d141bb4e407faea13bfc
Mark Turchik <mark.turchik@outlook.com>
2023-12-27 10:49:20 -0600

    Update Validator to allow longer Item Names

--- WarcraftGearPlanner.Server/Services/Items/ItemValidator.cs
+++ WarcraftGearPlanner.Server/Services/Items/ItemValidator.cs
@@ -8,7 +8,7 @@ public class ItemValidator : AbstractValidator<Item>
 	public ItemValidator()
 	{
 		RuleFor(x => x.ItemId).NotEmpty();
-		RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+		RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
 		RuleFor(x => x.ItemClassId).NotEmpty();
 		RuleFor(x => x.ItemSubclassId).NotEmpty();
 	}
