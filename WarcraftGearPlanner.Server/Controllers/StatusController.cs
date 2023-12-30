0a3f570d043c6af71acd9e6893dcc2f38cbbe892
Mark Turchik <mark.turchik@outlook.com>
2023-12-30 16:06:06 -0600

    Use backup db connstring from environment variables

--- WarcraftGearPlanner.Server/Controllers/StatusController.cs
+++ WarcraftGearPlanner.Server/Controllers/StatusController.cs
@@ -3,13 +3,21 @@
 namespace WarcraftGearPlanner.Server.Controllers;
 [Route("api/[controller]")]
 [ApiController]
-public class StatusController : ControllerBase
+public class StatusController(IConfiguration configuration) : ControllerBase
 {
+	private readonly IConfiguration configuration = configuration;
+
 	[HttpGet]
 	[ProducesResponseType(StatusCodes.Status200OK)]
 	public ActionResult<string> GetStatus()
 	{
 		var version = GetType().Assembly.GetName().Version;
-		return Ok($"APPLICATION RUNNING - v{version}");
+		var configConnString = configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+		var envConnString = Environment.GetEnvironmentVariable("wgp-sql-db-conn-string");
+		var envConnString2 = configuration.GetValue<string>("wgp-sql-db-conn-string");
+		return Ok($"APPLICATION RUNNING - v{version} " +
+			$"- configConnString: {string.IsNullOrEmpty(configConnString)} " +
+			$"- envConnString: {string.IsNullOrEmpty(envConnString)} " +
+			$"- envConnString2: {string.IsNullOrEmpty(envConnString2)}");
 	}
 }
